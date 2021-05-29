2ee39c03db042809e983555d6cadc06fa1e86a70
Roylen John <roylen.599@gmail.com>
2021-05-29 23:35:27 +0530

    Linted mock server files

--- mock-server/server.js
+++ mock-server/server.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser')
 const jsonServer = require('json-server')
 const jwt = require('jsonwebtoken')
 
-const port = 8000;
+const port = 8000
 
 const server = jsonServer.create()
 const router = jsonServer.router('./database.json')
@@ -18,45 +18,51 @@ let userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
-server.use(jsonServer.defaults());
+server.use(jsonServer.defaults())
 
 const SECRET_KEY = '@JWTS3CR3T'
 
 const expiresIn = '1h'
 
-// Create a token from a payload 
+// Create a token from a payload
 function createToken(payload) {
   return jwt.sign(payload, SECRET_KEY, { expiresIn })
 }
 
-// Verify the token 
+// Verify the token
 function verifyToken(token) {
-  return jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ? decode : err)
+  return jwt.verify(token, SECRET_KEY, (err, decode) =>
+    decode !== undefined ? decode : err
+  )
 }
 
 // Check if the user exists in database
 function isAuthenticated({ emp_id, password }) {
   userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
-  return userdb.users.findIndex(user => user.emp_id === emp_id && user.password === password) !== -1
+  return (
+    userdb.users.findIndex(
+      (user) => user.emp_id === emp_id && user.password === password
+    ) !== -1
+  )
 }
 
 const noPassword = ({ password, ...rest }) => rest
 
-let access_token;
+let access_token
 // Register New User
 server.post('/auth/register', (req, res) => {
-  console.log("register endpoint called; request body:");
-  console.log(req.body);
-  const { emp_id, password, name } = req.body;
+  console.log('register endpoint called; request body:')
+  console.log(req.body)
+  const { emp_id, password, name } = req.body
 
   if (isAuthenticated({ emp_id, password }) === true) {
-    const status = 401;
-    const message = 'User already exist';
-    res.status(status).json({ status, message });
+    const status = 401
+    const message = 'User already exist'
+    res.status(status).json({ status, message })
     return
   }
 
-  fs.readFile("./users.json", (err, data) => {
+  fs.readFile('./users.json', (err, data) => {
     if (err) {
       const status = 401
       const message = err
@@ -65,59 +71,75 @@ server.post('/auth/register', (req, res) => {
     }
 
     // Get current users data
-    var data = JSON.parse(data.toString());
+    var data = JSON.parse(data.toString())
 
     // Get the id of last user
-    var last_item_id = data.users.length > 0 ? data.users[data.users.length - 1].id : 0;
+    var last_item_id =
+      data.users.length > 0 ? data.users[data.users.length - 1].id : 0
 
     //Add new user
-    const newUser = { id: last_item_id + 1, emp_id: emp_id, password: password, name: name }
-    data.users.push(newUser); //add some data
+    const newUser = {
+      id: last_item_id + 1,
+      emp_id: emp_id,
+      password: password,
+      name: name,
+    }
+    data.users.push(newUser) //add some data
     access_token = createToken(noPassword(newUser))
 
-    var writeData = fs.writeFileSync("./users.json", JSON.stringify(data), (err, result) => {  // WRITE
-      if (err) {
-        const status = 401
-        const message = err
-        res.status(status).json({ status, message })
-        return
+    var writeData = fs.writeFileSync(
+      './users.json',
+      JSON.stringify(data),
+      (err, result) => {
+        // WRITE
+        if (err) {
+          const status = 401
+          const message = err
+          res.status(status).json({ status, message })
+          return
+        }
       }
-    });
+    )
 
     // Create token for new user
-    console.log("Access Token:" + access_token);
+    console.log('Access Token:' + access_token)
     res.status(200).json({ access_token })
-  });
+  })
 })
 
 // Login to one of the users from ./users.json
 server.post('/auth/login', (req, res) => {
-  console.log("login endpoint called; request body:");
-  console.log(req.body);
-  const { emp_id, password } = req.body;
+  console.log('login endpoint called; request body:')
+  console.log(req.body)
+  const { emp_id, password } = req.body
   if (isAuthenticated({ emp_id, password }) === false) {
     const status = 401
     const message = 'Incorrect username or password'
     res.status(status).json({ status, message })
     return
   }
-  const user = userdb.users.find(user => user.emp_id === emp_id && user.password === password)
-  console.log('User found:', user);
+  const user = userdb.users.find(
+    (user) => user.emp_id === emp_id && user.password === password
+  )
+  console.log('User found:', user)
   const access_token = createToken(noPassword(user))
-  console.log("Access Token:" + access_token);
+  console.log('Access Token:' + access_token)
   res.status(200).json({ access_token })
 })
 
 server.use(/^(?!\/auth).*$/, (req, res, next) => {
-  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+  if (
+    req.headers.authorization === undefined ||
+    req.headers.authorization.split(' ')[0] !== 'Bearer'
+  ) {
     const status = 401
     const message = 'Error in authorization format'
     res.status(status).json({ status, message })
     return
   }
   try {
-    let verifyTokenResult;
-    verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+    let verifyTokenResult
+    verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1])
 
     if (verifyTokenResult instanceof Error) {
       const status = 401
